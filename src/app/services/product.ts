b872dc823a3427fca5389403aab26aeeaeb7fa67
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-11-24 21:31:19 -0500

    as

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -11,7 +11,7 @@ export class ProductService {
   private http = inject(HttpClient);
 
   // URL basada en tu 'application.properties' (puerto 3005) y 'ProductController' (/api/products)
-  private apiUrl = 'https://ensayodespliegue.onrender.com';
+  private apiUrl = 'http://localhost:3005/api/products';
 
   /**
    * Obtener todos los productos
